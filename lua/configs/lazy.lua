96bf158983a4b1ced74dce1368aad16cedb4d4eb
孔令飞 <konglingfei.nosbelm@bytedance.com>
2025-04-02 13:55:36 +0800

    feat: commit at 20250402135536
    Change-Id: I6c7315bcaefeac8331fbc065816a44bbd66821a8

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -51,7 +51,7 @@ require("lazy").setup({
     { import = "plugins/notify" },
     { import = "plugins/avante" }, 
     { import = "plugins/root" },   
-    { import = "plugins/blame" },   
+    -- { import = "plugins/blame" },   
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
